5e331328dd19ab1dd712796895d880af87e46fb6
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 <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>
2021-07-26 18:12:14 +0545

    added

--- vars/Amazonetl.groovy
+++ vars/Amazonetl.groovy
@@ -13,7 +13,15 @@ pipeline {
     gpg_trust = credentials("gpg-trust")
     gpg_passphrase = credentials("gpg-password")
  }
-  
+  stages {
+    
+    stage('checkout git') {
+                steps {
+                    git credentialsId: 'f73bdad1-1506-4ee0-82f1-eab94378d8a5', url: 'https://github.com/sagarshrestha24/etl.git'
+                }
+            }
+   
+ 
  stage('Building a image for amazon-associate-etl ') {
       when {
         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
@@ -120,4 +128,4 @@ pipeline {
 
 }
 }
-
+}
