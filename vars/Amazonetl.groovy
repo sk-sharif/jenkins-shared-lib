ce0926af4906efdbfaea78108b04d29f9086b47e
sk-sharif <60419485+sk-sharif@users.noreply.github.com>
2021-07-27 13:36:49 +0530

    Update Amazonetl.groovy

--- vars/Amazonetl.groovy
+++ vars/Amazonetl.groovy
@@ -18,19 +18,53 @@ pipeline {
     
    
  
-//  stage('Building a image for amazon-associate-etl ') {
+ stage('Building a image for amazon-associate-etl ') {
+      when {
+        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
+      }
+      steps {
+        script {
+          docker.withRegistry('', registryCredential) {
+            sh '''ls
+                pwd
+            '''
+              sh '''
+               cd amazon-associate-etl/docker-images/amazon-associate-service/
+               make build-image '''
+
+          }
+        }
+
+      }
+      
+    }
+//     stage('Test a image for  amazon-associate-etl ') {
+//       when {
+//         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
+//       }
+//       steps {
+//         script {
+          
+//             sh '''
+//                cd amazon-associate-etl/docker-images/amazon-associate-service/
+//                make test-image '''
+
+          
+//         }
+
+//       }
+      
+//     }
+//     stage('Push a image amazon-associate-etl ') {
 //       when {
 //         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
 //       }
 //       steps {
 //         script {
 //           docker.withRegistry('', registryCredential) {
-//             sh '''ls
-//                 pwd
-//             '''
-//               sh '''
+//             sh '''
 //                cd amazon-associate-etl/docker-images/amazon-associate-service/
-//                make build-image '''
+//                make push-image '''
 
 //           }
 //         }
@@ -38,92 +72,58 @@ pipeline {
 //       }
       
 //     }
-    stage('Test a image for  amazon-associate-etl ') {
-      when {
-        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
-      }
-      steps {
-        script {
+//     stage('Pre-deploy image for amazon-associate-etl ') {
+//       when {
+//         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
+//       }
+//       steps {
+//         script {
           
-            sh '''
-               cd amazon-associate-etl/docker-images/amazon-associate-service/
-               make test-image '''
+//             sh '''
+//                cd amazon-associate-etl/docker-images/amazon-associate-service/
+//                make pre-deploy-image '''
 
-          
-        }
+//         }
+//         }
 
-      }
-      
-    }
-    stage('Push a image amazon-associate-etl ') {
-      when {
-        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
-      }
-      steps {
-        script {
-          docker.withRegistry('', registryCredential) {
-            sh '''
-               cd amazon-associate-etl/docker-images/amazon-associate-service/
-               make push-image '''
-
-          }
-        }
-
-      }
-      
-    }
-    stage('Pre-deploy image for amazon-associate-etl ') {
-      when {
-        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
-      }
-      steps {
-        script {
-          
-            sh '''
-               cd amazon-associate-etl/docker-images/amazon-associate-service/
-               make pre-deploy-image '''
-
-        }
-        }
-
-      }
+//       }
       
     
-    stage('deploy image for amazon-associate-etl ') {
-      when {
-        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
-      }
-      steps {
-        script {
-          docker.withRegistry('', registryCredential) {
-            sh '''
-               cd amazon-associate-etl/docker-images/amazon-associate-service/
-               make deploy-dockerimage '''
+//     stage('deploy image for amazon-associate-etl ') {
+//       when {
+//         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
+//       }
+//       steps {
+//         script {
+//           docker.withRegistry('', registryCredential) {
+//             sh '''
+//                cd amazon-associate-etl/docker-images/amazon-associate-service/
+//                make deploy-dockerimage '''
 
-          }
-        }
+//           }
+//         }
 
-      }
+//       }
       
-    }
-    stage('Post-deploy image for amazon-associate-etl ') {
-      when {
-        branch 'test'
-        changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
-      }
-      steps {
-        script {
+//     }
+//     stage('Post-deploy image for amazon-associate-etl ') {
+//       when {
+//         branch 'test'
+//         changeset "amazon-associate-etl/docker-images/amazon-associate-service/**"
+//       }
+//       steps {
+//         script {
         
-            sh '''
-               cd amazon-associate-etl/docker-images/amazon-associate-service/
-               make post-deploy-image '''
+//             sh '''
+//                cd amazon-associate-etl/docker-images/amazon-associate-service/
+//                make post-deploy-image '''
 
           
-        }
+//         }
 
-      }
+//       }
       
-    }
+//     }
 
 }
 }
